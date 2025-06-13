ef33108cb8f6a1de60d1e2c38247ae026d300aaa
Prabhdeep Singh <sprabhdeep024@gmail.com>
2025-06-13 11:58:58 +0530

    Initial commit

---
+++ src/test/java/com/khalsa/gurshabadlangar/GurshabadLangarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalsa.gurshabadlangar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurshabadLangarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
